48952235db8d177639a515e096e87f9c98b9f7f7
ellie Chen <>
2022-04-28 03:55:06 +0000

    I put comments on each page of the edits I made.

--- part3.py
+++ part3.py
@@ -7,8 +7,13 @@ There are (at least) 6 errors in this code. Fix them so that it runs properly.
 '''
 
 #code starts here
-number1 = input("Enter a number: ")
-number2 = int(input("Enter another number: ")
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
 
-print "The sum of your numbers is", number1 + Number2
-print(Seven times your second number is, 7(number2))
+print("The sum of your numbers is", number1 + number2)
+print("Seven times your second number is", 7 * number2)
+
+# i added int before input in line 10
+# i closed the parenthesis in line 11
+# i added parentheses after print in line 13, closed the space there, and also made number2 lowercase
+# i added quotes where they were necessary and removed the extra parentheses in line 14
